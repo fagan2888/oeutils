245537c95cd87719694e44e502047fcc5ff9c820
josuebrunel <josuebrunel@gmail.com>
2013-03-23 22:56:43 +0100

    It's a good wrapper now

--- README.md
+++ README.md
@@ -1,16 +1,61 @@
 ##OEUTILS
 
-__oeutils__ is a small python wrapper of the xml-rpc for OpenERP. It is usefull to manipulate your OpenERP objects.
+__oeutils__ is a small python wrapper of the xml-rpc API for OpenERP. It is usefull to manipulate your OpenERP objects.
+It is not as powerful as __erppeek__ yet but i hope that with your help one day it will ^_^
 
-####Features
+###Features
 With __oeutils__ you are able to :
 
 . Create objects
 . Read objects
 . Update objects
 . Delete objects
+. Search objects
+. Search models
+. Get models fields
 
 
-####How to use
+###How to
 
+####Connexion
 
+	:::python
+
+	>>>from oeutils import Openerp
+  	>>>o = Openerp(login,pwd,dbname)
+  	>>>o.connect()
+
+####Methods
+
+	:::python
+	>>>m = 'family.family'
+	>>>ids = o.search(m,[]) # or juste o.search(m)
+	>>>ids
+	[1, 2]
+	>>>
+	>>> famliy = {
+	... 'name': 'kouka',
+	... }
+    >>> o.create('family.family',famliy)
+    2
+    >>>m = 'family.family'
+    >>>o.read(m,ids,[])
+    [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'kouka', 'id': 2}]
+	>>>m = 'family.family'
+    >>> o.edit(m,2,{'name':'koukson'})
+    True
+    >>> o.read(m,o.search(m,[]),[])
+    [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'koukson', 'id': 2}]
+	>>>m = 'family.family'
+    >>> o.read(m,o.search(m,[]),[])
+    [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'koukson', 'id': 2}, {'member_ids': [], 'name': 'kim', 'id': 3}]
+    >>> o.delete(m,3)
+    True
+    >>> o.read(m,o.search(m,[]),[])
+    [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'koukson', 'id': 2}]
+     >>> o.models('family')
+	['family.family', 'family.member']
+	>>> o.keys(m)
+    ['member_ids', 'name']
+
+That's all, thanks in advance for your feebacks
--- oeutils.py
+++ oeutils.py
@@ -1,3 +1,8 @@
+###############################################
+# -*- coding: utf-8 -*-
+# Kéba ! na libossooo !! ngué ké diata farata 
+###############################################
+
 import xmlrpclib
 
 class Openerp(object):
@@ -10,14 +15,20 @@ class Openerp(object):
        port : server port i.e 8069
 
     Usage:
-      >>>from oetuils import Openerp
+      >>>from oeutils import Openerp
       >>>oe = Openerp(login,pwd,dbname)
       >>>oe.connect()
 
       Now you ready to go
     '''
     
-    def __init__(self, login, pwd, dbname, host='localhost', port=8069):
+    _DEFAULT_HOST_ = 'localhost'
+    _DEFAULT_LOGIN_ = 'admin'
+    _DEFAULT_PASSWORD_ = 'admin'
+    _DEFAULT_DBNAME_ = 'openerp'
+    _DEFAULT_PORT_ = 8069
+    
+    def __init__(self, login=_DEFAULT_LOGIN_, pwd=_DEFAULT_PASSWORD_, dbname=_DEFAULT_DBNAME_, host=_DEFAULT_HOST_, port=_DEFAULT_PORT_):
         self.host = host
         self.dbname = dbname
         self.login = login
@@ -30,6 +41,10 @@ class Openerp(object):
     def __unicode__(self,):
         return "Connected on %s with user %s " %(self.host,self.login)
 
+    #########################################################
+    # SERVER
+    ########################################################
+        
     def sock_common(self,):
         self.common = xmlrpclib.ServerProxy('http://%s:%s/xmlrpc/common' %(self.host,self.port))
         return self.common
@@ -47,14 +62,21 @@ class Openerp(object):
     def getUid(self,):
         return self.uid
 
+    def execute(self,):
+        pass
+        
     ########################################################
     # OPERATIONS       
     ########################################################
 
-    def search(self,model,attrs):
+    def search(self,model,attrs=[]):
         '''
         Returns the list ids matching the filters
         attrs is list.
+        Example:
+        >>>ids = o.search('family.family',[])
+        >>>ids
+        [1, 2]
         '''
         ids = self.sock.execute(self.dbname, self.uid, self.pwd, model, 'search', attrs)
         return ids
@@ -63,36 +85,86 @@ class Openerp(object):
         '''Create a new record object
         'attrs' is dictionary
         return the id of the object created
+        Example:
+        >>> famliy = {
+        ... 'name': 'kouka',
+        ... }
+        >>> o.create('family.family',famliy)
+        2
         '''
         id = self.sock.execute(self.dbname, self.uid, self.pwd, model, 'create', attrs)
         return id
 
-    def read(self, model, ids, attrs):
-        '''
+    def read(self, model, ids, attrs=[]):
+        '''Returns list of records matching the search ids
+           according to filters.
+        Example:
+        >>>m = 'family.family'
+        >>>o.read(m,ids,[])
+        [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'kouka', 'id': 2}]
         '''
         elts = self.sock.execute(self.dbname, self.uid, self.pwd, model, 'read', ids, attrs)
         return elts
 
-    def edit(self, model, attrs):
-        '''
+    def edit(self, model, ids, attrs):
+        '''Updates records and return True of records updated
+           attrs is dictionary
+        Example:
+        >>>m = 'family.family'
+        >>> o.edit(m,2,{'name':'koukson'})
+        True
+        >>> o.read(m,o.search(m,[]),[])
+        [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'koukson', 'id': 2}]
         '''
         ids = self.sock.execute(self.dbname, self.uid, self.pwd, model, 'write', ids, attrs)
         return ids
         
-    def delete(self, model, ids, attrs):
-        '''
+    def delete(self, model, ids):
+        '''Delete records and returns True if deleted
+        Example:
+        >>>m = 'family.family'
+        >>> o.read(m,o.search(m,[]),[])
+        [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'koukson', 'id': 2}, {'member_ids': [], 'name': 'kim', 'id': 3}]
+        >>> o.delete(m,3)
+        True
+        >>> o.read(m,o.search(m,[]),[])
+        [{'member_ids': [1], 'name': 'loking', 'id': 1}, {'member_ids': [], 'name': 'koukson', 'id': 2}]
+        >>> 
         '''
         ids = self.sock.execute(self.dbname, self.uid, self.pwd, model, 'unlink', ids)
         return ids
 
-    def model(self, model):
-        '''Search a model and return matching model or models
-        '''
-        
+
+    #########################################################
+    # Maybe the only interesting part, not clean and clear
+    # though
     ########################################################
+    def models(self, modelname):
+        '''Search a model and return matching model or models
+        Example:
+        >>> o.models('family.family')
+        ['family.family']
+        '''
+        ids = self.search('ir.model', [('model', 'ilike', modelname)])
+        results = [ m['model'] for m in self.read('ir.model', ids, ['model','name'])]
+        return results
 
-    
+    def keys(self, modelname):
+        '''Returns list of fields of a model
+        Example:
+        >>> m = 'family.family'
+        >>> o.keys(m)
+        ['member_ids', 'name']
+        >>> 
+        '''
+        ids = self.search('ir.model.fields', [('model','=',modelname)])
+        results = [ m['name'] for m in self.read('ir.model.fields', ids, ['name'])]
+        return results
 
-
+    #Will i be able to implement a browse method ?
+ 
+    ########################################################
+    ########################################################
+        
 if __name__ == '__main__':
     openerp = Openerp()
